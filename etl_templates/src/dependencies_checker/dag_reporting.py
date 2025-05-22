746f6ae19166d21962daddb2cfeb55e45637c9f9
Mark Zwart <m_zwart@TVMoebius.onmicrosoft.com>
2025-05-22 15:22:37 +0200

    Path shizzle

--- etl_templates/src/dependencies_checker/dag_reporting.py
+++ etl_templates/src/dependencies_checker/dag_reporting.py
@@ -175,7 +175,8 @@ class DagReporting(DagGenerator):
         net.toggle_physics(True)
         for edge in net.edges:
             edge["shadow"] = True
-        net.show(file_html, notebook=False)
+        file = str(Path(file_html).resolve())
+        net.show(name=file, notebook=False)
 
     def _dag_node_hierarchy_level(self, dag: ig.Graph) -> ig.Graph:
         """Enrich the DAG with the level in the hierarchy where vertices should be plotted.
