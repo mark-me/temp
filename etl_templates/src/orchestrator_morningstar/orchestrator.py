a5bfd5b711111a2fc962de2e741b704796207c51
Mark Zwart <mark.zwart@pobox.com>
2025-08-15 22:28:05 +0200

    Start review morningstar

--- etl_templates/src/orchestrator_morningstar/orchestrator.py
+++ etl_templates/src/orchestrator_morningstar/orchestrator.py
@@ -1,22 +1,26 @@
-import os
 from pathlib import Path
 
 from integrator import EtlSimulator
 from logtools import get_logger
 from config import MorningstarConfig
-from tqdm import tqdm
+
 
 logger = get_logger(__name__)
 
 class Orchestrator:
-    
-    def __init__(self):
+
+    def __init__(self, path_config: Path):
         """
-            
+        Initialiseert een nieuwe Orchestrator instantie met het opgegeven configuratiepad.
+
+        Slaat het pad naar het configuratiebestand op voor later gebruik.
+
+        Args:
+            path_config (Path): Pad naar het configuratiebestand.
         """
-        super().__init__()
-    
-    def build_dag(file_config: str) -> EtlSimulator:
+        self.path_config = path_config
+
+    def build_dag(self,) -> EtlSimulator:
         """
         Initialiseert een nieuwe ReportEtlSimulation instantie met het opgegeven configuratiebestand.
 
@@ -25,23 +29,15 @@ class Orchestrator:
         Args:
             file_config (str): Pad naar het configuratiebestand.
         """
-        file_config = Path(file_config)
-        config = MorningstarConfig(file_config=file_config)
+        config = MorningstarConfig(file_config=self.path_config)
         logger.info(
-            f"Failure report geïnitialiseerd met configuratie uit '{file_config}'"
+            f"Failure report geïnitialiseerd met configuratie uit '{self.path_config}'"
         )
-        paths_RETW = []
-        dag = EtlSimulator()
 
-        paths_RETW = []
-        path = config.path_input 
-        files_RETW = os.listdir(path)
-        print(files_RETW)
-        for file_retw in tqdm(
-            files_RETW, desc="Extracten Repo Json bestanden", colour = "#d7f5cb"
-        ):
-            logger.info("Aanvullen input files met folderlocatie ")
-            file_retw = config.path_input / file_retw
-            paths_RETW.append(file_retw)
+        dag = EtlSimulator()
+        paths_RETW = [x for x in config.path_input.iterdir() if x.is_file()]
+        if not paths_RETW:
+            logger.error(f"Geen Power Designer extracts gevonden in {config.path_input}")
+            return None
         dag.build_dag(files_RETW=paths_RETW)
         return dag
