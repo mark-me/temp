7848f9a8d94d133d2d02ddb0a453294856a5a8dd
Mark Zwart <mark.zwart@pobox.com>
2025-08-24 21:32:21 +0200

    Removed redundant code

--- etl_templates/src/pd_extractor/stereotype_extractor.py
+++ etl_templates/src/pd_extractor/stereotype_extractor.py
@@ -100,22 +100,3 @@ class StereotypeExtractor(BaseExtractor):
             if key in stereotype_object:
                 stereotype_object.pop(key)
                 logger.debug(f"Verwijderd {key} voor lst_objects in {self.file_pd_ldm}")
-
-    def _domains(self) -> dict | None:
-        """Extraheert alle domeinen die in het Power Designer model zijn gedefinieerd voor stereotypes.
-
-        Deze functie zoekt naar domeinen in het model en retourneert een dictionary met domeininformatie,
-        of None als er geen domeinen zijn gevonden.
-
-        Returns:
-            dict | None: Een dictionary met domeinen of None als er geen domeinen zijn gevonden.
-        """
-        path_keys = ["c:Domains", "o:Domain"]
-        if lst_domains := self._get_nested(data=self.content, keys=path_keys):
-            dict_domains = self.transform_stereotype.domains(lst_domains=lst_domains)
-            return dict_domains
-        else:
-            logger.error(
-                f"Er is geen gebruik van Domain geconstateerd in '{self.file_pd_ldm}' voor het extraheren van een stereotype"
-            )
-            return None
